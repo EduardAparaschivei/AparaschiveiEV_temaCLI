4b3dd52c994b38061652cbd958449dc1570991b1
EduardAparaschivei <eduard.aparaschivei@student.usv.ro>
2024-11-08 11:40:57 +0200

    ok

---
+++ AparaschiveiEV_tema02/Eduard_Test/Eduard_Test.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using OpenTK.Platform;
+
+namespace Eduard_Test
+{
+     class SimpleWindow : GameWindow
+    {
+        private float PosX = 0.0f;
+        private float PosY = 0.0f;
+        public SimpleWindow() : base(800, 600)
+        {
+          
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(Color.MidnightBlue);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+
+            //transformările care urmează vor afecta matricea de modelare și vizualizare, nu matricea de proiecție.
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+            GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            //verificare apasare tasta sageata stanga
+           if(Keyboard.GetState().IsKeyDown(Key.Left))
+            {
+                PosX -= 0.01f; //deplasare stanga
+            }
+           if(Keyboard.GetState().IsKeyDown(Key.Right))
+            {
+                PosX += 0.01f;//deplasare dreapta
+            }
+            if (Keyboard.GetState().IsKeyDown(Key.Down))
+            {
+                PosY -= 0.01f; //deplasare stanga
+            }
+            if (Keyboard.GetState().IsKeyDown(Key.Up))
+            {
+                PosY += 0.01f;//deplasare dreapta
+            }
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.PushMatrix();//salvare matrice curenta
+
+            //Se aplica transformarile
+            GL.Translate(PosX, PosY, 0.0f);
+
+            GL.Begin(PrimitiveType.Triangles);
+
+            GL.Color3(Color.MidnightBlue);
+            GL.Vertex2(-1.0f, 1.0f);
+            GL.Color3(Color.SpringGreen);
+            GL.Vertex2(0.0f, -1.0f);
+            GL.Color3(Color.Ivory);
+            GL.Vertex2(1.0f, 1.0f);
+
+            GL.End();
+
+            GL.PopMatrix();//Restaurare matrice
+
+            this.SwapBuffers();
+        }
+
+        protected override void OnMouseMove(MouseMoveEventArgs e)
+        {
+            //Modificare pozitie pe verticala
+            //Am pus '-' deoarece Y este inversat de OpenGL fata de coordonata Y a ferestrei
+            PosY = -(float)e.Y / Height;
+            PosX = (float)e.X / Width * 2.0f - 1.0f;
+        }
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using(SimpleWindow example = new SimpleWindow())
+            {
+                example.Run(30.0, 0.0);
+            }
+        }
+     }
+}
---
+++ AparaschiveiEV_tema02/Eduard_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Eduard_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Eduard_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b4759125-99dc-4c78-b51f-9d9dda97a0d7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
